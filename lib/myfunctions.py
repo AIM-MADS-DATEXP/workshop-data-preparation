827c972c95dfd3ce570c01ccbed00939e6318c9f
mcdejonge <matthijs@rommelhok.com>
2025-03-24 15:12:36 +0100

    Added notebook on programming techniques

---
+++ lib/myfunctions.py
@@ -0,0 +1,7 @@
+# This is a silly example of a file that contains functions you can reuse in your projects.
+
+def add_one_to(value):
+    """
+    Simple function that adds 1 to a value.
+    """
+    return value + 1
